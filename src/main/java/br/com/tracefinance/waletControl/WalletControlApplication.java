002b2ccdbce2e1b09aa3296abea89cb070d949d0
yandrade1305 <yan13052001@gmail.com>
2022-06-27 13:29:13 -0300

    Subindo projeto inicial

---
+++ src/main/java/br/com/tracefinance/waletControl/WalletControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.tracefinance.waletControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletControlApplication.class, args);
+	}
+
+}
